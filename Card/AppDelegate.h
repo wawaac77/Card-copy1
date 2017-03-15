d4a9db7858216a00167ec4f46b8df58caed37ca2
Alice Jin <alicejin@pc-home-mac.local>
2017-03-15 15:39:43 +0800

    Initial Commit

---
+++ Card/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Card
+//
+//  Created by Alice Jin on 15/3/2017.
+//  Copyright © 2017 Alice Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Card/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Card
+//
+//  Created by Alice Jin on 15/3/2017.
+//  Copyright © 2017 Alice Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
